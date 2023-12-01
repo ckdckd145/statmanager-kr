192b1608be210973107f1f671b0b9131a696fb39
DESKTOP-CSHOME <ckdckd145@gmail.com>
2023-12-01 21:03:31 +0900

    Mistake..  __init__.py

--- statmanager/__init__.py
+++ statmanager/__init__.py
@@ -1,4 +1,6 @@
-from .manager import Stat_Manager
+from .menu_for_howtouse import *
+from .messages_for_reporting import *
+from .manager import *
 
 __version__ = '1.7.2'
 __all__ = ['Stat_Manager']
--- statmanager/setup.py
+++ statmanager/setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-with open(r'../README.md', encoding='utf-8') as f:
+with open(r'statmanager\README.md', encoding='utf-8') as f:
     description_markdown = f.read()
 
 setup(
